4619fb028db18afd762e59484c9c936db6cf1ad3
sayun <156537626+sayunlopchan@users.noreply.github.com>
2025-03-10 13:57:05 +0545

    Update server.js

--- Sparkle_by_Cultivate_your_canvas_backend/server.js
+++ Sparkle_by_Cultivate_your_canvas_backend/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors({
   // origin: "http://localhost:5173"
-  origin: "https://sparkle-nepal.onrender.com"
+  origin: "https://sparkle-nepal.vercel.app"
 }));
 
 // Middleware to parse JSON bodies
