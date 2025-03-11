7678dc42b02e8c547078efeb1bb01a8e54611fa6
sayunlopchan <sayunlopchan01+github@gmail.com>
2025-03-11 16:39:44 +0545

    tiny fixes

--- Sparkle_by_Cultivate_your_canvas_forntend/src/components/Booking.jsx
+++ Sparkle_by_Cultivate_your_canvas_forntend/src/components/Booking.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 const Booking = () => {
   return (
-    <div className='flex flex-wrap items-center justify-center md:gap-5 p-10 md:p-20 gap-5 bg-gradient-to-tr from-yellow-500 to bg-orange-500 text-start text-white'>
+    <div className='flex flex-wrap items-center justify-between md:gap-5 p-10 md:p-20 gap-5 bg-gradient-to-tr from-yellow-500 to bg-orange-500 text-start text-white'>
       <p className='text-sm md:text-xl lg:text-2xl'>
         Empower Young Minds with Art, Dance, Yoga, <br /> and More. Ready to Sparkle?
       </p>
